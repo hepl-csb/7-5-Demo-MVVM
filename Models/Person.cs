8b6f3bcd8f780cb84978bf1680c9b43688e3498e
Daniel <daniel.schreurs@hepl.be>
2022-03-18 10:32:56 +0100

    :tada: initial commit

---
+++ Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace Models
+{
+
+    public class Person:INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { 
+                _name = value;
+                PropertyChangedEventHandler(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        public Person(string name = "Default")
+        {
+            Name = name;
+        }
+        private void PropertyChangedEventHandler(Person person, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            PropertyChanged?.Invoke(person, propertyChangedEventArgs);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+    }
+}
---
+++ WpfApp/MainWindowViewModel .cs
@@ -0,0 +1,44 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WpfApp
+{
+    internal class MainWindowViewModel
+    {
+        public ObservableCollection<Person> People { get; set; }
+        private bool _upper;
+
+        public MainWindowViewModel() { 
+            
+            People = new ObservableCollection<Person>();
+            People.Add(new Person("Toto"));
+            People.Add(new Person("Titi"));
+            People.Add(new Person("Tata"));
+            People.Add(new Person("Tutu"));
+            _upper = false;
+        }
+        
+        public void ToggleCase()
+        {
+            foreach (Person person in People)
+            {
+              if (_upper)
+                {
+                    person.Name = person.Name.ToLower();
+                }
+                else
+                {
+                    person.Name = person.Name.ToUpper();
+                }
+                
+            }
+            _upper = !_upper;
+        }
+    }
+}
